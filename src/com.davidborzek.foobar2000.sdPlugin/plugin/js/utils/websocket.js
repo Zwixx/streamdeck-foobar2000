170f432a11e5ab0eeb780eadbc324f39d032b885
Zwixx <zwiixx@googlemail.com>
2023-09-26 19:08:32 +0200

    Deleted unsused functions

--- src/com.davidborzek.foobar2000.sdPlugin/plugin/js/utils/websocket.js
+++ src/com.davidborzek.foobar2000.sdPlugin/plugin/js/utils/websocket.js
@@ -1,51 +1,6 @@
 let intervals = {};
 
 const websocketUtils = {
-  registerPlugin: (uuid, event) => {
-    if (websocket) {
-      const data = {
-        event,
-        uuid,
-      };
-
-      websocket.send(JSON.stringify(data));
-    }
-  },
-  requestGlobalSettings: (context) => {
-    if (websocket) {
-      const data = {
-        event: "getGlobalSettings",
-        context,
-      };
-
-      websocket.send(JSON.stringify(data));
-    }
-  },
-  saveSettings: (action, context, payload) => {
-    if (websocket) {
-      const data = {
-        action,
-        event: "setSettings",
-        context,
-        payload,
-      };
-      websocket.send(JSON.stringify(data));
-    }
-  },
-  setTitle: (context, title) => {
-    if (websocket) {
-      const data = {
-        event: "setTitle",
-        context,
-        payload: {
-          title,
-          target: 0,
-        },
-      };
-
-      websocket.send(JSON.stringify(data));
-    }
-  },
 
   setAsyncTitle: (text, interval, context) => {
     text = ` ${text} `;
@@ -79,76 +34,5 @@ const websocketUtils = {
       currentFirstChar1++;
       currentFirstChar2++;
     }, interval);
-  },
-
-  setSettings: (context, settings) => {
-    if (websocket) {
-      const data = {
-        event: "setSettings",
-        context,
-        payload: settings,
-      };
-
-      websocket.send(JSON.stringify(data));
-    }
-  },
-  setState: (context, state) => {
-    if (websocket) {
-      const data = {
-        event: "setState",
-        context,
-        payload: {
-          state,
-        },
-      };
-
-      websocket.send(JSON.stringify(data));
-    }
-  },
-  showAlert: (context) => {
-    if (websocket) {
-      const data = {
-        event: "showAlert",
-        context,
-      };
-
-      websocket.send(JSON.stringify(data));
-    }
-  },
-  log: (message) => {
-    if (websocket) {
-      const data = {
-        event: "logMessage",
-        payload: {
-          message,
-        },
-      };
-
-      websocket.send(JSON.stringify(data));
-    }
-  },
-  openUrl: (url) => {
-    if (websocket) {
-      const data = {
-        event: "openUrl",
-        payload: {
-          url,
-        },
-      };
-
-      websocket.send(JSON.stringify(data));
-    }
-  },
-  setImage: (context, image) => {
-    if (websocket) {
-      const data = {
-        event: "setImage",
-        context,
-        payload: {
-          image
-        },
-      };
-      websocket.send(JSON.stringify(data));
-    }
   },
 };
